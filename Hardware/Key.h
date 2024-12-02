d9b2c8cd3a6d2e47da95ca7e4a679593a54b71a1
Name-CK <3126127106@qq.com>
2024-12-02 21:32:05 +0800

    UP Key

--- Hardware/Key.h
+++ Hardware/Key.h
@@ -1,10 +1,8 @@
 #ifndef _KEY_H_
 #define _KEY_H_
-#include "stm32f10x.h"                  // Device header
 
 void Key_Init(void);
-uint8_t Key_GetNum(uint8_t *State);//获取键码
-
 void Key_Tick(void);//更新键码;将该函数加入定时器中断函数
+uint8_t Key_GetNum(uint8_t *State);//获取键码
 
 #endif
